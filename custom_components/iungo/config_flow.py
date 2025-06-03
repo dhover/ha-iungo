f36a4343281167dbbb39334223fced25153dd420
Dinant Scholte in 't Hoff <dinant@dtwm.nl>
2025-06-03 20:05:59 +0200

    gui config_flow

--- custom_components/iungo/config_flow.py
+++ custom_components/iungo/config_flow.py
@@ -1,23 +1,45 @@
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_HOST
+from homeassistant.core import callback
 import voluptuous as vol
+from .const import DOMAIN, CONF_HOST
 
 class IungoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Iungo."""
+    """Handle a config flow for the Iungo integration."""
+
+    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_HOST], data=user_input)
+:
+            # Prevent duplicate entries for the same host
+            await self.async_set_unique_id(user_input[CONF_HOST])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=user_input[CONF_HOST],
+                data=user_input,
+            )
 
         data_schema = vol.Schema({
             vol.Required(CONF_HOST): str,
         })
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=errors
+            step_id="user",
+            data_schema=data_schema,
+            errors=errors,
         )
 
-    async def async_step_import(self, import_info):
-        """Import a config entry from YAML."""
-        return self.async_create_entry(title="Iungo", data=import_info)
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return IungoOptionsFlowHandler(config_entry)
+
+class IungoOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options for Iungo."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return self.async_create_entry(title="", data={})
