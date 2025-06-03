d9a2737b43c5e531171fe29f8b060198d23a1b21
Dinant Scholte in 't Hoff <dinant@dtwm.nl>
2025-06-03 20:23:03 +0200

    Fix async_setup_platforms error

--- custom_components/iungo/__init__.py
+++ custom_components/iungo/__init__.py
@@ -1,5 +1,4 @@
 from .const import DOMAIN
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 async def async_setup(hass, config):
     """Set up the iungo integration."""
@@ -12,13 +11,9 @@ async def async_setup_entry(hass, entry):
     coordinator = IungoDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
