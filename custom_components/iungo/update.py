c3b3922f487117c51ba95f842c086a3e9c178373
Dinant <dinant@dtwm.nl>
2025-08-03 10:50:39 +0200

    Add firmware version sensors for Iungo Hub

--- custom_components/iungo/update.py
+++ custom_components/iungo/update.py
@@ -4,10 +4,15 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .coordinator import IungoDataUpdateCoordinator
 from .const import DOMAIN
+from homeassistant.components.sensor import SensorEntity
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([IungoUpdateEntity(coordinator)])
+    async_add_entities([
+        IungoUpdateEntity(coordinator),
+        IungoFirmwareVersionSensor(coordinator),
+        IungoLatestFirmwareVersionSensor(coordinator)
+    ])
 
 class IungoUpdateEntity(UpdateEntity):
     def __init__(self, coordinator: IungoDataUpdateCoordinator):
@@ -42,4 +47,48 @@ class IungoUpdateEntity(UpdateEntity):
             "hw_version": hwinfo.get("revision", ""),
             "sw_version": f"{sw_version} build {build}".strip(),
             "serial_number": serial_number
+        }
+
+class IungoFirmwareVersionSensor(SensorEntity):
+    def __init__(self, coordinator: IungoDataUpdateCoordinator):
+        super().__init__()
+        self.coordinator = coordinator
+        self._attr_name = "Iungo Firmware Version"
+        self._attr_unique_id = f"{coordinator.entry.entry_id}_firmware_version"
+        self._attr_icon = "mdi:tag"
+
+    @property
+    def native_value(self):
+        version = self.coordinator.sysinfo.get("version", {}) if self.coordinator.sysinfo else {}
+        return version.get("version")
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.coordinator.entry.entry_id)},
+            "name": "Iungo Hub",
+            "manufacturer": "Iungo",
+            "model": "Iungo",
+        }
+
+class IungoLatestFirmwareVersionSensor(SensorEntity):
+    def __init__(self, coordinator: IungoDataUpdateCoordinator):
+        super().__init__()
+        self.coordinator = coordinator
+        self._attr_name = "Iungo Latest Firmware Version"
+        self._attr_unique_id = f"{coordinator.entry.entry_id}_latest_firmware_version"
+        self._attr_icon = "mdi:tag-arrow-up"
+
+    @property
+    def native_value(self):
+        version = self.coordinator.latest_version.get("fw", {}) if self.coordinator.latest_version else {}
+        return version.get("version")
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.coordinator.entry.entry_id)},
+            "name": "Iungo Hub",
+            "manufacturer": "Iungo",
+            "model": "Iungo",
         }
